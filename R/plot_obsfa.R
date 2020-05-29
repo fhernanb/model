c5dfe6b5959dac226af3996965ff543d41891cfe
fhernanb <fhernanb@unal.edu.co>
2020-05-29 10:04:38 -0500

    se arregló un problemita

--- R/plot_obsfa.R
+++ R/plot_obsfa.R
@@ -9,19 +9,7 @@
 #' @param lchar the number of first characters to identify points, by default is 3.
 #' @param ... arguments to be passed to methods, such as graphical parameters (see par).
 #' 
-#' @examples
-#' v1 <- c(1,1,1,1,1,1,1,1,1,1,3,3,3,3,3,4,5,6)
-#' v2 <- c(1,2,1,1,1,1,2,1,2,1,3,4,3,3,3,4,6,5)
-#' v3 <- c(3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,5,4,6)
-#' v4 <- c(3,3,4,3,3,1,1,2,1,1,1,1,2,1,1,5,6,4)
-#' v5 <- c(1,1,1,1,1,3,3,3,3,3,1,1,1,1,1,6,4,5)
-#' v6 <- c(1,1,1,2,1,3,3,3,4,3,1,1,1,2,1,6,5,4)
-#' m1 <- cbind(v1,v2,v3,v4,v5,v6)
-#' scores <- factanal(m1, factors=3, method="mle",
-#'                    scores="regression")$scores
-#' 
-#' plot_obsfa(scores, fx=1, fy=2, col='blue', pch=20)
-#' plot_obsfa(scores, fx=1, fy=3, col='red', pch=19)
+#' @example inst/examples/examples_plot_obsfa.R
 #' @return \code{plot_obsfa} function returns a plot with the observations in the coordinate plane Fi-Fj.
 #' 
 #' @details The number in \code{fx} and \code{fy} must be less or equal
@@ -30,7 +18,7 @@
 #' @importFrom graphics plot text abline
 #' @export
 plot_obsfa <- function(scores, fx=1, fy=2, lchar=3, ...) {
-  stopifnot(class(scores) == 'matrix')
+  stopifnot(any(class(scores) == 'matrix'))
   if (is.null(rownames(scores))) {
     n <- dim(scores)[1]
     rownames(scores) <- 1:n
