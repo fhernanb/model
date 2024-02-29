16bcfb6777fccfad332a874dfa5911df982f66aa
fhernanb <fhernanb@unal.edu.co>
2024-02-29 18:21:58 -0500

    se mejoró la función

--- R/est_param.R
+++ R/est_param.R
@@ -5,19 +5,32 @@
 #' @param mod a gamlss object.
 #' @param fun the estimated parameter, by default is mean but can be change for sd or var.
 #' @param m a number to indicate the number of observations to simulate, by default its values is 100.
+#' @param data the original data frame used to fit mod.
+#' @param newdata a data frame containing new values for the explanatory variables.
 #' 
 #' @example examples/examples_est_param.R
 #' @return \code{est_param} function returns a vector.
 #' 
 #' @details The function obtains the fitted values for mu, sigma, nu and tau. The functions simulates m observations and then it obtains the mean/fun defined by the user.
 #' 
-#' @import gamlss
+#' @importFrom stats predict
 #' @export
-est_param <- function(mod, fun="mean", m=100) {
-  stopifnot(class(mod)[1] == 'gamlss')
-  aux <- paste0("mod$", mod$parameters, ".fv")
-  aux <- paste0("cbind(", paste(aux, sep="", collapse=", "), ")")
-  fit <- eval(parse(text=aux))
+est_param <- function(mod, fun="mean", m=100, data, newdata=NULL) {
+  stopifnot(class(mod)[1] == "gamlss")
+  
+  if (is.null(newdata)) {
+    aux <- paste0("mod$", mod$parameters, ".fv")
+    aux <- paste0("cbind(", paste(aux, sep="", collapse=", "), ")")
+    fit <- eval(parse(text=aux))
+  }
+  else {
+    fit <- NULL
+    for (i in mod$parameters) {
+      temp <- predict(mod, what=i, data=data, newdata=newdata, type="response")
+      fit <- cbind(fit, temp)
+    }
+  }
+  # Auxiliar function to simulate
   generator <- function(x) {
     aux <- paste0("r", mod$family[1], "(n=m, ", paste(x, sep="", collapse=", "), ")")
     eval(parse(text=aux))
